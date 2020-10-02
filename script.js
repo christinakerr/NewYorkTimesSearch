79f2e311eb4b2b419f0ee33847c3fa8a65dcb82d
Christi Kerr <christinakerr394@gmail.com>
2020-10-02 13:45:10 -0500

    Phase 2 start

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ var query = "Titanic";
 var beginDate = "1912";
 var endDate = "1914";
 
-var queryURL = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=d7bYASqiWzVjLRRbENP2bqfftB2jG7NY"
-    + "/articlesearch.json?q=" + query + "&begin_date=" + beginDate + "0101&end_date=" + endDate + "1231";
+var queryURL = "https://api.nytimes.com/svc/archive/v1/2019/1.json?"
+    + "/articlesearch.json?q=" + query + "&begin_date=" + beginDate + "0101&end_date=" + endDate + "1231&api-key=d7bYASqiWzVjLRRbENP2bqfftB2jG7NY";
+
+console.log(queryURL);
 
 
 function queryFunction() {
